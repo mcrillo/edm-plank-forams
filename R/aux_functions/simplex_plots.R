e0ed440ea186af433371750782db9008371d41ba
Marina Costa Rillo <marina.rillo@evobio.eu>
2018-05-11 15:42:10 +0200

    simplex function

---
+++ R/aux_functions/simplex_plots.R
@@ -0,0 +1,56 @@
+
+
+simplex_plot <- function(data_ts, embed_max, trap_name, overwrite){    
+  
+  
+  if(overwrite == TRUE | !file.exists(paste("output/",trap_name,"/simplex_pred_plots",sep=""))){
+    
+    if (!file.exists(paste("output/",trap_name,"/simplex_pred_plots",sep=""))){ dir.create(paste("output/",trap_name,"/simplex_pred_plots/",sep="")) }
+    
+  
+ rho_df <- data.frame()
+
+ for (i in 2:ncol(data_ts)){
+  
+  X <- as.data.frame(data_ts)[,i]
+  pred <- simplex(X, lib = c(1, NROW(X)), pred = c(1, NROW(X)), E = embed_max[i-1,"emax"], tp = 1:20, stats_only = FALSE, silent = T)
+  fits <- pred$model_output[[1]]
+  # head(fits)
+  
+  rho_df[i,"variable"] <- colnames(data_ts)[i]
+  rho_df[i,"rho"] <- round(pred$rho[1],3)
+  rho_df[i,"emax"] <- embed_max[i-1,"emax"]
+  
+  savename1<-paste("output/",trap_name,"/simplex_pred_plots/",colnames(data_ts)[i], "_pred.png",sep="")
+  png(savename1, width = 800, height = 500)
+  plot(pred ~ time, data = fits, type = "l", col = "blue", lwd=3,
+       xlab="Time", ylab=colnames(data_ts)[i], ylim=range(fits[,2:3],na.rm = T))
+  lines(obs ~ time, data = fits, col=grey.colors(1, alpha=0.25), lwd = 6)
+  legend("topright", c("Observed", "Predicted"), lty=1, lwd=c(6,3),
+         col=c(grey.colors(1, alpha=0.25), "blue"),bty="n")
+  dev.off()
+  
+  savename2<-paste("output/",trap_name,"/simplex_pred_plots/decay_",colnames(data_ts)[i], ".png",sep="")
+  png(savename2, width = 800, height = 600)
+  plot(rho ~ tp, data=pred,
+       type = "b",
+       xlab = "Time to prediction",
+       ylab = expression(paste("Forecast skill (",rho,")",sep="")))
+  dev.off()
+  
+}
+rho_df <- rho_df[-1,]
+
+write.csv(rho_df, paste("output/",trap_name,"/simplex_rho_",trap_name,".csv",sep=""), row.names = F)
+return(rho_df)
+
+
+  }else{
+    
+    rho_df <- read.csv(paste("output/",trap_name,"/simplex_rho_",trap_name,".csv",sep=""), header = TRUE, stringsAsFactors = FALSE)
+    return (rho_df)
+  }
+  
+  
+} # function
+
