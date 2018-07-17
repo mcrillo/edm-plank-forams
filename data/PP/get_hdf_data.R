c882ab6805bb1dc9f7e4ab8c33f7c19d571161b2
Marina Costa Rillo <marina.rillo@evobio.eu>
2018-07-17 11:46:36 +0200

    hdf problems

--- data/PP/get_hdf_data.R
+++ data/PP/get_hdf_data.R
@@ -1,10 +1,14 @@
 
 rm(list=ls())
 
+library(R.utils)
+
 setwd("/Users/marinacostarillo/Google Drive/PhD/projects")
 setwd("./time-series-forams/data/PP/")  
-files <- list.files()
+files <- list.files(pattern = ".gz")
 
+### hdf files 
+# gdalinfo("cbpm_2008001.hdf")
 install.packages("hdf5r") 
 # Problem:
 # hdf5r is only available in source form, and may need compilation of C/C++/Fortran: 'hdf5r' 
@@ -13,6 +17,8 @@ install.packages("hdf5r")
 # Apple is moving away from libstdc++, the system-provided libstdc++ has not been updated since OS X 10.7 (Lion) and it lacks any C++11 facility.
 # http://ease-the-computation.haunted.io/compile-clang-against-libstdc-with-c11-support-on-a-mac/
 
-
-gdalinfo("cbpm_2008001.hdf")
-
+### xyz files: http://orca.science.oregonstate.edu/2160.by.4320.8day.xyz.vgpm.m.chl.m.sst.php
+# week 2008097 http://orca.science.oregonstate.edu/data/2x4/8day/vgpm.r2018.m.chl.m.sst/xyz/vgpm.2008097.all.xyz.gz
+i = 1
+gunzip(files[i])
+vgpm.2008097.all.xyz.gz
